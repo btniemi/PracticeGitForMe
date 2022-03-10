d21af61b1e92c34728868b4f27ed388d498e7794
Braedon Niemi <70549370+btniemi@users.noreply.github.com>
2022-03-10 15:57:36 -0700

    comments
    did comments for code

--- arrowHeight(SWL).py
+++ arrowHeight(SWL).py
@@ -28,24 +28,27 @@
 # problem.
 
 def arrow(n):
+    # i chose to separate the 2 cases of even (n) given or odd (n) given and handle them separately
     if n%2 == 0:
-        for i in range(0, n//2):
+        # for printing upper part of arrow if n == even
+        for i in range(0, n//2): # I had the right process for splitting just not implemented quite right. The use of floor division helped
             for j in range(0, i):
-                print(" ", end='')
+                print(" ", end='') #prints spaces up to where need the \ to be places
             print("\\")
-
-        for i in range(n//2, 0, -1):
+        # for printing the lower part
+        for i in range(n//2, 0, -1): # this is like same loop above but just in reverse allows us to access peak at right point and go down from there ie other side of arrow
             for j in range(0, i-1):
                 print(" ", end='')
             print("/")
     else:
+        # printing top part if n == odd
         for i in range(0, n // 2):
             for j in range(0, i):
                 print(" ", end='')
             print("\\")
-
-        for x in range(n // 2, -1, -1):
-            for y in range(0, x+1):
+        # printing botton part of arrow
+        for x in range(n // 2, -1, -1): # <-- iterates from n//2 to -1 one extra the amount of times to get bottom half example if n=5 then top have loops 2times and this bottom half loops 3
+            for y in range(0, x+1): # --> goes from 0 to how many times above loops for
                 if y == n//2:
                     print(">")
                 elif y == x:
@@ -53,6 +56,7 @@ def arrow(n):
                 else:
                     print(" ", end='')
 
+#calls to function to print out arrows
 arrow(1)
 print("separator for new arrow print")
 arrow(2)
